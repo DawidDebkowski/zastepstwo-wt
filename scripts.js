4c9acd1127334a94dfeb49519361503a2ad4b5fd
memizzos <4321nitro1234@gmail.com>
2019-05-16 19:45:37 +0200

    Koniec zajec

--- scripts.js
+++ scripts.js
@@ -7,19 +7,36 @@ const ranges = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
 
 function toggleVideo() {
-        if (video.paused) {
-            video.play();
-        }
-        else {
-            video.pause();
-        }
+    if (video.paused) {
+        video.play();
+    }
+    else {
+        video.pause();
+    }
 }
 
-progress.addEventListener("click", function(e){
+progress.addEventListener("click", function (e) {
     video.currentTime = e.offsetX / progress.offsetWidth * video.duration;
 });
+// sprawdzenie czy mysza jest juz nie na pasku
+
+// progress.addEventListener("mousemove", function (e) {
+//     if (e.mousedown){
+//     video.currentTime = e.offsetX / progress.offsetWidth * video.duration;}
+// });
+
 toggle.addEventListener("click", toggleVideo);
+
 video.addEventListener("click", toggleVideo);
-video.addEventListener("timeupdate", function(){
+
+video.addEventListener("timeupdate", function () {
     progressBar.style.flexBasis = video.currentTime / video.duration * 100 + "%";
 });
+
+ranges.forEach(range => {
+    const setValue = function(){
+        video[this.name] = this.value;
+    }
+    range.addEventListener("change",setValue);
+    range.addEventListener("mousemove", setValue);
+})
