fb1028adaf0dbf51a7100e2e168f34f99de4a3d4
memizzos <4321nitro1234@gmail.com>
2019-05-16 18:43:38 +0200

    Zatrzymywanie i odtwarzenie filmu

--- scripts.js
+++ scripts.js
@@ -5,3 +5,15 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const ranges = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
+
+function toggleVideo() {
+        if (video.paused) {
+            video.play();
+        }
+        else {
+            video.pause();
+        }
+}
+
+toggle.addEventListener("click", toggleVideo);
+video.addEventListener("click", toggleVideo);
